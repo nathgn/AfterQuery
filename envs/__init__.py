48c2c0190c70ae045817eff30183f53c8e013245
nathan <nathanguan@berkeley.edu>
2026-03-25 23:03:49 -0700

    remove unnecessary fikes

--- envs/__init__.py
+++ envs/__init__.py
@@ -1,4 +1,2 @@
-from envs.terminalbench_client import TerminalBenchClient, TaskState
+from envs.terminalbench_client import TerminalBenchClient
 from envs.terminalbench_env import TerminalBenchEnv
-
-__all__ = ["TerminalBenchClient", "TaskState", "TerminalBenchEnv"]
